c4839b151f3857f572ae0f25f0f786fcf7af27b2
Girish Bhide <51797025+gbhide1993@users.noreply.github.com>
2025-12-18 18:28:44 +0530

    Update worker_merchant.py

--- worker_merchant.py
+++ worker_merchant.py
@@ -6,7 +6,7 @@ from rq import Worker, Queue
 # ADD THIS LINE: Tell the worker to look in the current directory for tasks_merchant.py
 sys.path.append(os.getcwd())
 
-listen = ['default']
+listen = ['merchant_jobs']
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
@@ -15,3 +15,4 @@ if __name__ == '__main__':
     queues = [Queue(name, connection=conn) for name in listen]
     worker = Worker(queues, connection=conn)
     worker.work()
+
